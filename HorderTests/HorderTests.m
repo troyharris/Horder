9b653ca9e81fcd4bc18d0b027bd6676ea1239665
Troy HARRIS <troy.harris@djt-tech.com>
2013-08-06 12:01:59 -0700

    Initial Commit

---
+++ HorderTests/HorderTests.m
@@ -0,0 +1,34 @@
+//
+//  HorderTests.m
+//  HorderTests
+//
+//  Created by Troy HARRIS on 8/6/13.
+//  Copyright (c) 2013 Lone Yeti. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HorderTests : XCTestCase
+
+@end
+
+@implementation HorderTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
